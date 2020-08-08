5046a141ce14d96ca3a355cc86f364747d714f2a
Mark Miller <mark@zhukmedia.com>
2020-08-08 12:10:52 -0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: function(h) {
+  store,
+
+  render: function (h) {
     return h(App);
   }
 }).$mount("#app");
